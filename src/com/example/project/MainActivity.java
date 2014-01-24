a2eeb704c3166dcbb88f42a3dd794bfb6c9acf48
Elwira Lichocka <ariwle.lichocka@gmail.com>
2014-01-24 12:32:26 +0100

    diagram klas i male zmiany

--- src/com/example/project/MainActivity.java
+++ src/com/example/project/MainActivity.java
@@ -1,15 +1,57 @@
 package com.example.project;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import database.AppDbFacade;
+import database.AppDbHelper;
 
 public class MainActivity extends Activity {
 
+    private static AppDbHelper dbOpenHelper = null;
+    public static AppDbFacade dbHelper = null;
+	private ListView options;
+	private String [] menuOptions = {"Add New Task", "Ongoing Tasks", "History"};
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.menu);
+		 setupDbEnv();
+		options = (ListView) findViewById(R.id.list_menu);
+		options.setAdapter(new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, menuOptions));
+		options.setOnItemClickListener(new OnItemClickListener() {
+
+		    public void onItemClick(AdapterView<?> parent, View view, int position,
+		            long id) {
+		    	Intent intent = new Intent();
+		    	switch(position) {
+		    	case 0:	    			
+		    			intent.setClass(view.getContext(), AddTaskActivity.class);
+		    			startActivity(intent);
+		    			break;
+		    	case 1:
+	    				intent.setClass(view.getContext(), OngoingActivity.class);
+	    				startActivity(intent);
+		    			break;
+		    	case 2:
+		    			intent.setClass(view.getContext(), HistoryActivity.class);
+		    			startActivity(intent);
+		    			break;
+		    	default:
+		    			break;
+		    	}
+		    	Log.i("topics","d "+position);
+		    }
+		}); 
 	}
 
 	@Override
@@ -19,4 +61,13 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	  private void setupDbEnv() {
+		  Log.i("topics.database","setup!");
+	        if (dbOpenHelper == null) {
+	            dbOpenHelper = new AppDbHelper(this);
+	        } 
+	        if (dbHelper == null) {
+	            dbHelper = new AppDbFacade(dbOpenHelper.getWritableDatabase());
+	        }
+	    }
 }
